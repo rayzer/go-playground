147988c89c3c08b6578e355062d4403b47858b3d
Ray <ray.li@nokia-sbell.com>
2018-12-21 15:22:10 +0800

    p848

--- leet/p848/shiftingLetters_test.go
+++ leet/p848/shiftingLetters_test.go
@@ -1,13 +1,27 @@
+//https://leetcode.com/contest/weekly-contest-88/problems/shifting-letter
+//https://golang.org/ref/spec#Conversions
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func shiftingLetters(S string, shifts []int) string {
-	return "rpl"
+	for i := len(shifts) - 2; i >= 0; i-- {
+		shifts[i] = shifts[i] + shifts[i+1]
+	}
+	result := []byte(S)
+	for i := 0; i < len(result); i++ {
+		result[i] = byte((int(result[i])-'a'+shifts[i])%26 + 'a')
+	}
+	return string(result)
 }
 
 func Test(t *testing.T) {
 	if shiftingLetters("abc", []int{3, 5, 9}) != "rpl" {
+		t.Fail()
+	}
+	if shiftingLetters("bad", []int{10, 20, 30}) != "jyh" {
 		t.Fail()
 	}
 }
